6eedb6c2251aac8a1357fd825f94163c5a3c4484
fikribasa <flick_irst@rocketmail.com>
2019-08-15 22:44:34 +0700

    commit kedua

--- week1/2_2_arraynumber.js
+++ week1/2_2_arraynumber.js
@@ -1,32 +1,11 @@
-// function jumlahSatu(num){
-//     numSplit = []; //numInt = [];
-//     // numSplit= [...num+''].map(n=>+n);
-//     numSplit =num.map(String);
-//  //   numInt = numSplit.parseInt(Text, 10);
-// };
-
-// console.log(jumlahSatu([123,9,27]));
-
-
-// //kurang array split
-
-// function jumlahSatu(num){
-//     numSplit = []; newNum=[];
-//     numSplit =num.toString(10).split('');
-//     console.log(numSplit);
-    
-//     var newArr = numSplit.join(',').replace(/,/g, '').split();
-//     return newArr;	
-// };
-
 function jumlahSatu(num){
    
     newNum = num.reduce((a,b)=>a+b);
     let arr=newNum;
     
-    while (newNum >=10){
+    while (newNum >=9){
         console.log(arr);
-        arr = Array.from(String(newNum), Number);
+        arr = [Array.from(String(newNum), Number)];
         console.log(arr);
         newNum = arr.reduce((a,b)=>a+b);
         arr=newNum;
